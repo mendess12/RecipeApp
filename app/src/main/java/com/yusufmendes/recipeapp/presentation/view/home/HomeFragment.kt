ec743179c2675802d97e60a77010283941fe5a9a
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-04-25 22:00:12 +0300

    Add home screen design

---
+++ app/src/main/java/com/yusufmendes/recipeapp/presentation/view/home/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.yusufmendes.recipeapp.presentation.view.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.yusufmendes.recipeapp.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
