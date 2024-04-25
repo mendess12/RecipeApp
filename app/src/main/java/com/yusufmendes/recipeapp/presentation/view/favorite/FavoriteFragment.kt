e5f42bf9ad8bfe3387ee6cb080ee4592650c9bea
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-04-25 22:07:43 +0300

    Add view binding

---
+++ app/src/main/java/com/yusufmendes/recipeapp/presentation/view/favorite/FavoriteFragment.kt
@@ -0,0 +1,18 @@
+package com.yusufmendes.recipeapp.presentation.view.favorite
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.yusufmendes.recipeapp.R
+import com.yusufmendes.recipeapp.databinding.FragmentFavoriteBinding
+
+class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
+
+    private lateinit var binding: FragmentFavoriteBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentFavoriteBinding.bind(view)
+    }
+
+}
--- app/src/main/java/com/yusufmendes/recipeapp/presentation/view/home/HomeFragment.kt
+++ app/src/main/java/com/yusufmendes/recipeapp/presentation/view/home/HomeFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.yusufmendes.recipeapp.R
+import com.yusufmendes.recipeapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
     }
 }
