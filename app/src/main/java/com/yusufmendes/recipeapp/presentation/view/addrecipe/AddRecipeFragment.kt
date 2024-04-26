5de6dcc4612c9a9183049c7f27d45f7b38bcfba2
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-04-26 18:36:42 +0300

    Add recipe screen design

---
+++ app/src/main/java/com/yusufmendes/recipeapp/presentation/view/addrecipe/AddRecipeFragment.kt
@@ -0,0 +1,19 @@
+package com.yusufmendes.recipeapp.presentation.view.addrecipe
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.yusufmendes.recipeapp.R
+import com.yusufmendes.recipeapp.databinding.FragmentAddRecipeBinding
+
+class AddRecipeFragment : Fragment(R.layout.fragment_add_recipe) {
+
+    private lateinit var binding: FragmentAddRecipeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentAddRecipeBinding.bind(view)
+
+        binding.addRecipeTl.tlTitleTv.setText(R.string.add_recipe)
+    }
+}
