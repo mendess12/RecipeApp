44340dda59823900f75020d4600848a593cc2311
Yusuf Mendeş <yusufmendes123@gmail.com>
2024-04-26 21:22:24 +0300

    Add change password screen design

---
+++ app/src/main/java/com/yusufmendes/recipeapp/presentation/view/changepassword/ChangePasswordFragment.kt
@@ -0,0 +1,19 @@
+package com.yusufmendes.recipeapp.presentation.view.changepassword
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.yusufmendes.recipeapp.R
+import com.yusufmendes.recipeapp.databinding.FragmentChangePasswordBinding
+
+class ChangePasswordFragment : Fragment(R.layout.fragment_change_password) {
+
+    private lateinit var binding: FragmentChangePasswordBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentChangePasswordBinding.bind(view)
+
+        binding.changePasswordTl.passwordTlTitleTv.setText(R.string.change_password)
+    }
+}
